228adb74b620f56ae1b6bfdd415475d77d0e494d
Ben <ben24france@gmail.com>
2025-01-06 13:29:14 +0100

    1st

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/img/logo-nerf.png" type="image/x-icon">
+    <title>Nerf</title>
+</head>
+<body>
+    <header>
+        <div>
+            <img class="logo" src="assets/img/logo-nerf.png" alt="Logo Nerf">
+            <h1>Nerf</h1>
+            <span></span>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#1">Gamme 1</a></li>
+                <li><a href="#2">Gamme 2</a></li>
+                <li><a href="#3">Gamme 3</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <main>
+        <section id="1">
+            <h2>Gamme</h2>
+            <div>
+                <img src="assets/img/dart-tag/Nerf_Speedswarm.jpg" alt="Photo d'un Nerf Speedswarm">
+                <div class="text">
+                    <h3>Nom du modèle</h3>
+                    <p>Cadence de tir</p>
+                    <p>Descriptif</p>
+                </div>
+            </div>
+
+            <p><a href="">Modifier</a> - <a href="">Supprimer</a></p>
+        </section>
+    </main>
+
+
+    <footer>
+        <img class="logo" src="assets/img/logo-nerf.png" alt="Logo Nerf">
+
+        <section>
+            <h2>A Propos</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam repudiandae expedita quaerat perspiciatis aspernatur error facere maxime! Magni quasi error iure ex et vel vero, animi quisquam temporibus, earum minus.</p>
+
+        </section>
+
+        <a href=""><i class="fa-solid fa-arrow-up"></i></a>
+    </footer>
+
+    
+<!-- 
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░
+░░░░░░░░▄▀░░░░░░░░░░░░▄░░░░░░░▀▄░░░░░░░
+░░░░░░░░█░░▄░░░░▄░░░░░░░░░░░░░░█░░░░░░░
+░░░░░░░░█░░░░░░░░░░░░▄█▄▄░░▄░░░█░▄▄▄░░░
+░▄▄▄▄▄░░█░░░░░░▀░░░░▀█░░▀▄░░░░░█▀▀░██░░
+░██▄▀██▄█░░░▄░░░░░░░██░░░░▀▀▀▀▀░░░░██░░
+░░▀██▄▀██░░░░░░░░▀░██▀░░░░░░░░░░░░░▀██░
+░░░░▀████░▀░░░░▄░░░██░░░▄█░░░░▄░▄█░░██░
+░░░░░░░▀█░░░░▄░░░░░██░░░░▄░░░▄░░▄░░░██░
+░░░░░░░▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀░░
+░░░░░░█▀▀█████████▀▀▀▀████████████▀░░░░
+░░░░░░████▀░░███▀░░░░░░▀███░░▀██▀░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+ -->
+</body>
+</html>
